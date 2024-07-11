54f42d4b751778ce2d05c9c5be03bf7b96ba60bd
Marco Beduschi <marco.beduschi@nuuvem.com>
2024-07-11 09:09:58 -0300

    feat: validate password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :password, presence: true
+  validates :password, length: { in: 10..16 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,4 +14,18 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.valid?
     assert_includes user.errors.details[:password], { error: :blank }
   end
+
+  test 'password too short should be rejected' do
+    user = User.new(password: 'Abc123')
+
+    assert_not user.valid?
+    assert_includes user.errors.details[:password], { error: :too_short, count: 10 }
+  end
+
+  test 'password too long should be rejected' do
+    user = User.new(password: 'Abc12345678900000')
+
+    assert_not user.valid?
+    assert_includes user.errors.details[:password], { error: :too_long, count: 16 }
+  end
 end
