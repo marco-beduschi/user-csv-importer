42d609331f0a93f327d83a8f6649047909a09a27
Marco Beduschi <marco.beduschi@nuuvem.com>
2024-07-11 08:55:24 -0300

    feat: add user validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'name is required' do
+    user = User.new(name: nil)
+
+    assert_not user.valid?
+    assert_includes user.errors.details[:name], { error: :blank }
+  end
+
+  test 'password is required' do
+    user = User.new(password: nil)
+
+    assert_not user.valid?
+    assert_includes user.errors.details[:password], { error: :blank }
+  end
 end
