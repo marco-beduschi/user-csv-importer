e85930354214cb9c5bcd5277bc6f2c2e4b3b0bf0
Marco Beduschi <marco.beduschi@nuuvem.com>
2024-07-11 13:51:41 -0300

    test: add spec to controller

--- test/controllers/user_imports_controller_test.rb
+++ test/controllers/user_imports_controller_test.rb
@@ -16,4 +16,21 @@ class UserImportsControllerTest < ActionDispatch::IntegrationTest
       assert_select form, 'input[type="submit"]'
     end
   end
+
+  test '#create rerenders the form if invalid' do
+    post('/user_imports', params: { user_import: { file: nil } })
+
+    assert_response :unprocessable_entity
+
+    form = css_select('form')
+
+    assert form.attr('action'), '/user_imports'
+    assert form.attr('method'), 'post'
+
+    form.each do |form|
+      assert_select form, 'input[name="user_import[file]"]'
+      assert_select form, 'p', text: "File can't be blank"
+      assert_select form, 'input[type="submit"]'
+    end
+  end
 end
